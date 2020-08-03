fdc27d5abeca04afda8b8086bf8c87b196dcb8cb
TommyCox <55894704+TommyCox@users.noreply.github.com>
2020-08-03 10:24:44 -0600

    create tec500.py.

---
+++ tec500.py
@@ -0,0 +1,23 @@
+import pyvisa
+
+class TEC500():
+    def __init__(self):
+        pass
+
+    # Common Commands
+    # Clear Status
+    # Clears the Status Byte (STB) and the Event Status Register (ESR)
+
+    # System
+
+    # Laser
+
+    # Photodiode
+
+    # Current Coupling
+
+    # TEC
+
+    # Temperature Coupling
+
+    # Piezo
